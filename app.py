b794a2053be727e340dd6c7e8598b370a788f062
fraoucen <fraoucenkaci@gmail.com>
2022-12-24 17:06:13 +0100

    add new algorithm

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask,render_template
-import socket
-import pandas as pd
-
-
-
-
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 
 
 
@@ -12,27 +13,40 @@ app = Flask(__name__)
 
 def fitgen():
 
-    df = pd.read_csv("C:/Users/Froucen/Documents/MLDS 2/DE/Projet DE/app/Dummy Data HSS.csv" )
-
+    df = pd.read_csv("C:/Users/Froucen/Documents/MLDS 2/data engineering/Projet DE/app/Dummy Data HSS.csv" )
     df = df.fillna(df.mean())
     df = pd.get_dummies(df) 
     df = df[['TV', 'Radio', 'Social Media', 'Influencer_Macro',
        'Influencer_Mega', 'Influencer_Micro', 'Influencer_Nano', 'Sales']]
+    x = df.iloc[:,0:-1].values
+    y = df.iloc[:,-1:].values
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    lr_regressor = LinearRegression() 
+    lr_regressor.fit(x_train, y_train) 
+    y_pred_lr = lr_regressor.predict(x_test)  
+    scoreRegression = r2_score(y_test, y_pred_lr)
 
 
- 
-    return (df)
+
+    rf_regressor = RandomForestRegressor() 
+    rf_regressor.fit(x_train, y_train) 
+    y_pred_rf = rf_regressor.predict(x_test) 
+    scoreRandomForest = r2_score(y_test, y_pred_rf)
+
+    return [scoreRegression, scoreRandomForest]
 
 
 
-@app.route("/")
+@app.route("/", )
 def index():
-    
+   
+    list = fitgen()
+    scoreRegression = list[0]
+    scoreRandomForest = list[1]
 
-    return render_template('index.html', 
-
-    )
+    return render_template('index.html', scoreRegressionHtml = scoreRegression , scoreRandomHtml = scoreRandomForest )
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True )
