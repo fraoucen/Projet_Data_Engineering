26132bab98f1a512ac13a3dda0a220b70034d58f
aghiles <aghiles-izouaouen@hotmail.com>
2022-12-25 20:33:46 +0100

    Fix bug app.py && Data && update index.html

--- app.py
+++ app.py
@@ -15,33 +15,32 @@ app = Flask(__name__)
 
 def fitgen():
 
-    df = pd.read_csv("/app/data/Dummy_Data_HSS.csv")
+    df = pd.read_csv("/home/aghiles/projets/Projet_Data_Engineering/data/Dummy_Data_HSS.csv")
 
     df = df.fillna(df.mean())
     df = pd.get_dummies(df) 
-    df = df[['TV', 'Radio', 'Social Media', 'Influencer_Macro',
-       'Influencer_Mega', 'Influencer_Micro', 'Influencer_Nano', 'Sales']]
+    df = df[['TV', 'Radio', 'Social Media', 'Influencer_Macro','Influencer_Mega', 'Influencer_Micro', 'Influencer_Nano', 'Sales']]
     x = df.iloc[:,0:-1].values
     y = df.iloc[:,-1:].values
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y )
     lr_regressor = LinearRegression() 
     lr_regressor.fit(x_train, y_train) 
     y_pred_lr = lr_regressor.predict(x_test)  
-    scoreRegression = r2_score(y_test, y_pred_lr)
+    scoreRegression = round(r2_score(y_test, y_pred_lr),3)
 
 
 
     rf_regressor = RandomForestRegressor() 
     rf_regressor.fit(x_train, y_train) 
     y_pred_rf = rf_regressor.predict(x_test) 
-    scoreRandomForest = r2_score(y_test, y_pred_rf)
+    scoreRandomForest = round(r2_score(y_test, y_pred_rf),3)
 
 
     dt_regressor = DecisionTreeRegressor()
-    dt_regressor.fit(x_train, y_test)
+    dt_regressor.fit(x_train, y_train)
     y_pred_dt = dt_regressor.predict(x_test)
-    scoreDecisionTree = r2_score(y_test, y_pred_dt)
+    scoreDecisionTree = round(r2_score(y_test, y_pred_dt),3)
 
 
 
