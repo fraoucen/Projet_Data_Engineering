aa66c1982211328809493c33b78bc8c8563dcf97
Claaaas <claashmd37@gmail.com>
2022-12-25 16:56:28 +0100

    debug file app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 def fitgen():
 
-    df = pd.read_csv("C:/Users/Froucen/Documents/MLDS 2/data engineering/Projet DE/app/Dummy Data HSS.csv" )
+    df = pd.read_csv("C:/Users/Claas/Downloads/Dummy Data HSS.csv" )
     df = df.fillna(df.mean())
     df = pd.get_dummies(df) 
     df = df[['TV', 'Radio', 'Social Media', 'Influencer_Macro',
